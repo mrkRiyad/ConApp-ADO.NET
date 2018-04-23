b5f34afba1ab2b4e026c9729e2bb1990eeb27315
Riyad Khan <dev.riyad@gmail.com>
2018-04-24 00:21:09 +0600

    Setup C# Console configuration

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConApp_ADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
